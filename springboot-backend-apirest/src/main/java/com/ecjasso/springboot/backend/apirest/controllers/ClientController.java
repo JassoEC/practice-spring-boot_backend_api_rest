fd912205f86283ac1384f08677da5105c0e76a01
Emanuel Campos Jasso <emanuel.campos@sulivan.com.mx>
2021-07-30 09:52:30 -0500

    Se restringio el cors al dominio local

--- springboot-backend-apirest/src/main/java/com/ecjasso/springboot/backend/apirest/controllers/ClientController.java
+++ springboot-backend-apirest/src/main/java/com/ecjasso/springboot/backend/apirest/controllers/ClientController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ecjasso.springboot.backend.apirest.model.services.IClientService;
 import com.ecjasso.springboot.backend.apirest.models.entity.Client;
 
-@CrossOrigin(origins = { "http://localhost:3000" })
+@CrossOrigin(origins = { "http://localhost" })
 @RestController
 @RequestMapping("/api")
 public class ClientController {
