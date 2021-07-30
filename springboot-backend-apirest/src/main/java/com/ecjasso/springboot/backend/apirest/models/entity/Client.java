e14c5952a6897428c9b3b64a31e5c25ae1cea635
Emanuel Campos Jasso <emanuel.campos@sulivan.com.mx>
2021-07-30 09:37:57 -0500

    se corrigo la generacion de primary keys de la table clients

--- springboot-backend-apirest/src/main/java/com/ecjasso/springboot/backend/apirest/models/entity/Client.java
+++ springboot-backend-apirest/src/main/java/com/ecjasso/springboot/backend/apirest/models/entity/Client.java
@@ -17,7 +17,7 @@ import javax.persistence.TemporalType;
 public class Client implements Serializable {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String name;
--- springboot-backend-apirest/src/main/resources/import.sql
+++ springboot-backend-apirest/src/main/resources/import.sql
@@ -1,1 +1,6 @@
-insert into clients (id,name,last_name, email, created_at) values (2,"Emanuel", "Jasso", "isw.ecamposj@gmail.com","2020-03-06");
+insert into clients (name, last_name, email, created_at) values ('Michelle', 'Pacheco','michjelle@mail.com', '2020/07/30');
+insert into clients (name, last_name, email, created_at) values ('Emanuel', 'Campos','emanuel@mail.com', '2020/07/30');
+insert into clients (name, last_name, email, created_at) values ('Ana', 'Campos','ana@mail.com', '2020/07/30');
+insert into clients (name, last_name, email, created_at) values ('Jose', 'Campos','jose@mail.com', '2020/07/30');
+insert into clients (name, last_name, email, created_at) values ('Diego', 'Campos','diego@mail.com', '2020/07/30');
+insert into clients (name, last_name, email, created_at) values ('Luz', 'Campos','luz@mail.com', '2020/07/30');
