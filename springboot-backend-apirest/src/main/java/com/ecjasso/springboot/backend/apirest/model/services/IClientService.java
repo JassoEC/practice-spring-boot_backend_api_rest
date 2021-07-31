83dab3e3a457c2305c27627c2bcb663f3f6157ee
Emanuel Campos Jasso <emanuel.campos@sulivan.com.mx>
2021-07-31 09:53:02 -0500

    SE agregaron los metodos tradidcionales para un rest controller en el IClientDao

--- springboot-backend-apirest/src/main/java/com/ecjasso/springboot/backend/apirest/model/services/IClientService.java
+++ springboot-backend-apirest/src/main/java/com/ecjasso/springboot/backend/apirest/model/services/IClientService.java
@@ -7,4 +7,10 @@ import com.ecjasso.springboot.backend.apirest.models.entity.Client;
 public interface IClientService {
 
 	public List<Client> findAll();
+	
+	public Client findById(Long id);
+	
+	public Client save(Client client);
+	
+	public void delelte(Long id);
 }
