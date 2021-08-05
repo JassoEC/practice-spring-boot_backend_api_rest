4a92c4144e79cf39ef572cd0976667a050dc8c26
Emanuel Campos Jasso <emanuel.campos@sulivan.com.mx>
2021-08-05 10:38:16 -0500

    Se hizo la configuración de los Cors para permitir la conexión local del cliente de angular

--- springboot-backend-apirest/src/main/java/com/ecjasso/springboot/backend/apirest/controllers/ClientController.java
+++ springboot-backend-apirest/src/main/java/com/ecjasso/springboot/backend/apirest/controllers/ClientController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ecjasso.springboot.backend.apirest.model.services.IClientService;
 import com.ecjasso.springboot.backend.apirest.models.entity.Client;
 
-@CrossOrigin(origins = { "http://localhost" })
+@CrossOrigin(origins = { "http://localhost:4200" })
 @RestController
 @RequestMapping("/api")
 public class ClientController {
